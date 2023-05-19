cc517116f44d8c27cf9ba9a4aecc70130a6cc2d2
andritianaa <andritianasteve@gmail.com>
2023-05-19 19:56:18 +0300

    auth done

---
+++ front/src/components/auth.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import axios from "axios"
+const Auth = () => {
+    const [emailValue, setEmail] = useState("")
+    const [passwordValue, setPassword] = useState("")
+    const handleLogin = async () => {
+        if (!(emailValue && passwordValue)) { alert("Remplissez les deux champs") }
+        else {
+            await axios.post("http://localhost:8080/login", { email: emailValue, password: passwordValue })
+                .then((response) => {
+                    localStorage.setItem("id", JSON.stringify(response.data.id))
+                    alert(`Votre compte a été créé`)
+                    // window.location.href = "/scan"
+                }).catch(() => { alert("Le compte n'a pas été créé") })
+        }
+    }
+    const handleRegister = async () => {
+        if (!(emailValue && passwordValue)) { alert("Remplissez les deux champs") }
+        else {
+            await axios.post("http://localhost:8080/register", { email: emailValue, password: passwordValue })
+                .then((response) => {
+                    localStorage.setItem("id", JSON.stringify(response.data.id))
+                    alert(`Vous êtes connecté`)
+                    // window.location.href = "/scan"
+                }).catch(() => { alert("Vous avez entré un mauvais email ou un mauvais mot de passe") })
+        }
+    }
+
+    return (
+        <div className='card'>
+            <h2>Connectez-vous</h2>
+            <label htmlFor='email'>Email</label>
+            <input value={emailValue} onChange={(event) => setEmail(event.target.value)} type='email' id='email' placeholder='myemail@mail.org' required></input>
+            <label htmlFor='password'>password</label>
+            <input value={passwordValue} onChange={(event) => setPassword(event.target.value)} type='password' id='password' placeholder='•••••••••' required></input>
+            <button onClick={handleLogin}>Connexion</button>
+            <button onClick={handleRegister}>Inscription</button>
+        </div>
+    )
+}
+
+export default Auth
